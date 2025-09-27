013deb028c050e532da153f750797e5df81d61b0
Colin McGregor <100391145+colinwmc@users.noreply.github.com>
2025-09-27 18:43:39 +0200

    readying for netlify

--- Text_Game_Frontend/src/app/app.module.ts
+++ Text_Game_Frontend/src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -49,7 +49,7 @@ import { OutroComponent } from './outro/outro.component';
     FormsModule,
     ReactiveFormsModule
   ],
-  providers: [SharedService],
+  providers: [SharedService, provideClientHydration()],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
